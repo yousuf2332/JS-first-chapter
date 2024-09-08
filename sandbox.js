22a7560e4784501fb1cf3856306c187f68838820
yousuf2332 <yousuf23325268@gmail.com>
2024-09-08 10:58:34 -0700

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+alert("Error! Please enter restart");
+alert("Introduction to JS Land....\nHappy Coding!");
+alert("Introduction to JS Land....")
+alert("Happy Coding\nPrevent this page from creating additional dialogs.")
+
+var firstProgram = "Hello... I can run JS thorugh my web browser's console"
+console.log(firstProgram)
+alert(firstProgram)
